9ea5579169d118aab52c73a1d9fcffbee4ea43bb
didi40904 <116975925+didi40904@users.noreply.github.com>
2026-01-18 19:57:21 +0100

    Inicijalni Komit
    Prvi push (ima prijavu)

---
+++ Klijent/PodmorniceKlijent/Klijent.cs
@@ -0,0 +1,59 @@
+﻿using PodmorniceLibrary;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PodmorniceKlijent
+{
+    internal class Klijent
+    {
+        static void Main(string[] args)
+        {
+            bool prijavljen = false;
+            Igrac ja = new Igrac();
+            #region prijava
+            Console.WriteLine("==========KLIJENT JE POKRENUT==========");
+            Console.WriteLine("Pokrenuti prijavu na server? (prijava/ne): ");
+            string ans = Console.ReadLine();
+            if (ans.ToLower().Equals("prijava"))
+            {
+                Socket clientUDP = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                IPEndPoint destinationPoint = new IPEndPoint(IPAddress.Parse("192.168.2.108"), 15005); 
+                EndPoint clientPoint = new IPEndPoint(IPAddress.Any, 0);
+
+                BinaryFormatter bf = new BinaryFormatter();
+
+                while (!prijavljen)
+                {
+                    try
+                    {
+                        using (MemoryStream ms = new MemoryStream())
+                        {
+                            bf.Serialize(ms, ja);
+                            byte[] podaci = ms.ToArray();
+
+                            int brBajta = clientUDP.SendTo(podaci, 0, podaci.Length, SocketFlags.None, destinationPoint);
+                            prijavljen = true;
+                        }
+
+                    }
+                    catch (SocketException ex)
+                    {
+                        Console.WriteLine($"Doslo je do greske tokom slanja poruke: \n{ex}");
+                    }
+                }
+                #endregion prijava
+                clientUDP.Close();
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ PodmorniceServer/PodmorniceServer/Server.cs
@@ -0,0 +1,71 @@
+﻿using PodmorniceLibrary;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PodmorniceServer
+{
+    internal class Server
+    {
+        static void Main(string[] args)
+        {
+            int brojIgraca = -1;
+            int brojAktivnihIgraca = 0;
+            List<Igrac> aktivniIgraci = new List<Igrac>();
+
+
+            Console.WriteLine("==========SERVER JE POKRENUT==========");
+            #region prijave
+            do
+            {
+                Console.WriteLine("Unesite broj igraca (minimum 2): ");
+                brojIgraca = Int32.Parse(Console.ReadLine());
+            } while (brojIgraca < 2);
+
+            Socket serverUDP = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint serverPoint = new IPEndPoint(IPAddress.Parse("192.168.2.108"), 15005);  //15005 jer se lako pamti 
+            serverUDP.Bind(serverPoint);
+            byte[] recievingBuffer = new byte[1024];
+            EndPoint clientPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            Console.WriteLine("Prijave su otvorene. Cekam...");
+            BinaryFormatter bf = new BinaryFormatter();
+            while(brojAktivnihIgraca < brojIgraca)
+            {
+                try
+                {
+                    int brojBajta = serverUDP.ReceiveFrom(recievingBuffer, ref clientPoint);
+                    if (brojBajta == 0) break;
+                    string poruka = Encoding.UTF8.GetString(recievingBuffer, 0, brojBajta);
+
+                    using (MemoryStream ms = new MemoryStream(recievingBuffer, 0, brojBajta))
+                    {
+                        Igrac noviIgrac = (Igrac)bf.Deserialize(ms);
+                        Console.WriteLine("Prijava uspesna!");
+                        Console.WriteLine("Igrac broj: " + (brojAktivnihIgraca+1));
+                        noviIgrac.identifikacioniBroj = brojAktivnihIgraca + 1;  //svakom igracu ide redni broj ulaska na server, kao u minecraft mini igrama
+                        aktivniIgraci.Add(noviIgrac);
+                        brojAktivnihIgraca++;
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("\nGreska pri prijavi igraca! " + ex.Message);
+                }
+            }
+            Console.WriteLine("Prijave su zavrsene. Igra pocinje.");
+            #endregion prijave
+            //to do sve ostalo
+
+            serverUDP.Close();
+            Console.ReadKey();
+        }
+    }
+}
